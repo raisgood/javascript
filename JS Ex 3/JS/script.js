f9e8728e1e020b35d88a73b916407efc403f1107
raisgood <rashadhumphrey@hotmail.com>
2020-10-19 05:14:17 -0500

    upload

---
+++ JS Ex 3/JS/script.js
@@ -0,0 +1,15 @@
+function handleClick() {
+document.getElementById("dateTarget").innerHTML = Date();
+}
+
+document.getElementById('dateButton').onclick = handleClick;
+function handleClick() {
+document.getElementById('dateTarget').innerHTML = Date();
+}
+document.getElementById(dateButton').addEventListener('click', handleClick);
+function handleClick() {
+document.getElementById('dateTarget').innerHTML = Date();
+}
+document.getElementById('dateButton').addEventListener('click', function handleClick() {
+document.getElementById('dateTarget').innerHTML = Date();
+});
